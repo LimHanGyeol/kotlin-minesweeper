08321b5b043327e2dbd4b7a970af092b4994f280
LimHanGyeol <hang19663@gmail.com>
2022-01-02 20:06:59 +0900

    ✨ Mine 구현
    * 지뢰를 관리하는 Mine 구현

--- src/main/kotlin/minesweeper/domain/Mine.kt
+++ src/main/kotlin/minesweeper/domain/Mine.kt
@@ -1,6 +1,18 @@
 package minesweeper.domain
 
+import minesweeper.exception.InvalidMineException
+
 @JvmInline
 value class Mine(
     val count: Int,
-)
+) {
+    init {
+        if (count <= MIN_MINE_COUNT) {
+            throw InvalidMineException()
+        }
+    }
+
+    companion object {
+        private const val MIN_MINE_COUNT = 0
+    }
+}
